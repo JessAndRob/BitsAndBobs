2bda3c370d9f3fe3bfc969613d53342731231dce
jpomfret <jpomfret7@gmail.com>
2024-02-18 08:36:46 +0000

    we need a specific folder name to run like this

--- Scripts/savejumpykeepystuff.ps1
+++ Scripts/savejumpykeepystuff.ps1
@@ -1,18 +1,19 @@
 # ran for jumpy when we delete everything SQLBitsInfra so we don't lose our user settings
 $keepyFolder = 'C:\Users\keepyStuff\'
+$profileFolder = 'C:\Users\sqladmin.SQLBITS2024'
 
 # clear out keepyStuff
 Get-ChildItem $keepyFolder -recurse | remove-item -force -recurse
 
 # keep things from . folders
-if (Get-ChildItem ('{0}\.*' -f $env:USERPROFILE)) {
-  Copy-Item ('{0}\.*' -f $env:USERPROFILE) $keepyFolder -recurse
+if (Get-ChildItem ('{0}\.*' -f $profileFolder)) {
+  Copy-Item ('{0}\.*' -f $profileFolder) $keepyFolder -recurse
 }
 
 # keep anything on the desktop
-if (Get-ChildItem ('{0}\Desktop' -f $env:USERPROFILE)) {
+if (Get-ChildItem ('{0}\Desktop' -f $profileFolder)) {
   if(-not (test-path ('{0}\Desktop\*' -f $keepyFolder))) {
     New-Item ('{0}\Desktop\*' -f $keepyFolder) -Type Directory
   }
-  Copy-Item ('{0}\Desktop\*' -f $env:USERPROFILE) ('{0}\Desktop\*' -f $keepyFolder) -recurse
+  Copy-Item ('{0}\Desktop\*' -f $profileFolder) ('{0}\Desktop\*' -f $keepyFolder) -recurse
 }
